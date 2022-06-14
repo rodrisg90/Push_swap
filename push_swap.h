97af1f81db7efd9218d1d3b2a13032621eb337cd
rodrisg90 <102601255+rodrisg90@users.noreply.github.com>
2022-06-14 19:02:27 +0200

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -8,4 +8,18 @@
 # include <ctype.h>
 # include "libft.h"
 
+typedef struct s_Node
+{
+	int				data;
+	int				index;
+	int				true;
+	int				insertion;
+	int				operations;
+	struct s_Node	*next;
+}			t_Node;
+
+int		check_sorted(t_Node *head_ref);
+void	push_B(t_Node **b_Ref, t_Node **a_Ref);
+void	push_A(t_Node **b_Ref, t_Node **a_Ref);
+
 #endif
